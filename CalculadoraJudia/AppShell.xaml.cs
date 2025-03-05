63ebce6e5cc53b1bddf82dd7b61ee7651fa406a3
franc <franciscozs1221@petalmail.com>
2025-03-04 18:07:26 -0600

    Agregar archivos de proyecto.

---
+++ CalculadoraJudia/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraJudia
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CalculadoraJudia/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculadoraJudia
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        public MainPage(MainPageViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+
+
+    }
+
+}
